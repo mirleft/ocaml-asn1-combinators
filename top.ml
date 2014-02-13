fc7a7789acf92ef635998e29d387ab841ce1dbea
pqwy <unitedbiscuits@gmail.com>
2014-02-13 15:06:49 +0000

    script for setting up top environment

---
+++ top.ml
@@ -0,0 +1,10 @@
+(* #use this to setup the interactive environment. *)
+
+#require "num";;
+#require "oUnit";;
+
+#directory "_build/src";;
+#load "asn1-combinators.cma";;
+
+#directory "_build/tests";;
+#load "testlib.cma";;

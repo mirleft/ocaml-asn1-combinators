1bcb168a2e958da1e47dcb1e28ce44b1228cb245
pqwy <unitedbiscuits@gmail.com>
2014-02-21 19:01:51 +0000

    remove homebrew Cstruct mmap

--- tests/bench.ml
+++ tests/bench.ml
@@ -12,20 +12,15 @@ let time ?(iter=1) f =
     | n -> ignore (f ()) ; go (pred n) in
   measure @@ fun () -> go iter
 
-let mmap filename =
-  let fd  = Unix.(openfile filename [O_RDONLY] 0) in
-  let arr = Bigarray.(Array1.map_file fd char c_layout false (-1)) in
-  ( Unix.close fd ; Cstruct.of_bigarray arr )
-
 let bench_certs filename =
-  let arr = mmap filename in
-  let dec = Asn.decode_exn X509.cert_ber in
+  let cs = Unix_cstruct.of_fd @@ Unix.(openfile filename [O_RDONLY] 0)
+  and dec = Asn.decode_exn X509.cert_ber in
   let rec bench n cs =
     if Cstruct.len cs = 0 then n else
       let (a, cs') = dec cs in
       bench (succ n) cs' in
   time ~iter:1 @@ fun () ->
-    let n = bench 0 arr in
+    let n = bench 0 cs in
     Printf.printf "parsed %d cers.\n%!" n
 
 let _ = bench_certs "./rondom/certs.bin"
